09b2ed91d066e0a3f71687fdc2c05f35d4da4329
Muhamad Irwan <muhamadirwan99@gmail.com>
2024-09-07 19:41:29 +0700

    add cors setting

--- src/application/web.js
+++ src/application/web.js
@@ -2,8 +2,10 @@ import express from "express";
 import {publicRouter} from "../route/public-api.js";
 import {errorMiddleware} from "../middleware/error-middleware.js";
 import {userRouter} from "../route/api.js";
+import cors from "cors";
 
 export const web = express();
+web.use(cors());
 web.use(express.json());
 
 web.use(publicRouter);

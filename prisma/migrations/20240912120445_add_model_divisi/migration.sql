200dd10485c2eb265771af24e06a1db23c6382fc
Muhamad Irwan <muhamadirwan99@gmail.com>
2024-09-12 20:00:00 +0700

    api for divisi done

---
+++ prisma/migrations/20240912120445_add_model_divisi/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `Divisi` (
+    `kd_divisi` VARCHAR(10) NOT NULL,
+    `nm_divisi` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`kd_divisi`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/validation/role-validation.js
+++ src/validation/role-validation.js
@@ -17,6 +17,8 @@ const searchRoleValidation = Joi.object({
     page: Joi.number().min(1).positive().default(1),
     size: Joi.number().min(1).positive().max(100).default(10),
     role_name: Joi.string().optional(),
+    sort_by: Joi.array().optional(),
+    sort_order: Joi.array().optional()
 })
 
 export {

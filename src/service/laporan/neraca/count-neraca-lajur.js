82238a059ebdc862dcbbb2083765e06d9b91fed1
Muhamad Irwan <muhamadirwan99@gmail.com>
2025-02-21 13:21:37 +0700

    add feat neraca lajur(beban gaji, uang makan, thr, beban adm umum, beban perlengkapan, beban penyusutan inv)

---
+++ src/service/laporan/neraca/count-neraca-lajur.js
@@ -0,0 +1,16 @@
+async function getNeracaPercobaan(
+  neracaAwalDebit,
+  neracaAwalKredit,
+  neracaMutasiDebit,
+  neracaMutasiKredit,
+) {
+  const debit = neracaAwalDebit + neracaMutasiDebit;
+  const kredit = neracaAwalKredit + neracaMutasiKredit;
+
+  return {
+    debit: debit,
+    kredit: kredit,
+  };
+}
+
+export { getNeracaPercobaan };

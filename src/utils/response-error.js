b4c0a999ebdc8381372a1919cc00ff2d3d9db77a
Muhamad Irwan <muhamadirwan99@gmail.com>
2025-03-08 17:06:56 +0700

    add feat rekon stock take, history stock opname, improve handling error, improve log response db

--- src/utils/response-error.js
+++ src/utils/response-error.js
@@ -1,24 +1,25 @@
-import {logResponse} from "../service/log-service.js";
+import { logResponse } from "../service/log-service.js";
 
 class ResponseError extends Error {
+  constructor(message = "Error message", data = {}) {
+    super(message);
+    this.success = false;
+    this.data = data;
 
-    constructor(message = "Error message", data = {}) {
-        super(message);
-        this.success = false;
-        this.data = data;
+    logResponse(this.success, this.message, this.data).catch((err) => {
+      console.error("Error logging response:", err);
+    });
+  }
 
-        logResponse(this.success, this.message, this.data).catch(err => {
-            console.error("Error logging response:", err);
-        });
-    }
+  getResponse() {
+    const { username, ...dataWithoutUsername } = this.data;
 
-    getResponse() {
-        return {
-            success: this.success,
-            data: this.data,
-            message: this.message
-        };
-    }
+    return {
+      success: this.success,
+      data: dataWithoutUsername,
+      message: this.message,
+    };
+  }
 }
 
 export { ResponseError };
